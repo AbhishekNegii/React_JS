cf74b7ea550d4495e4c45b94d03c004219a736f2
Abhishek <abhibaba007@gmail.com>
2022-08-01 21:00:44 +0530

    First Component Created (Custom Expense Item Ele)

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,15 +1,15 @@
 function ExpenseItem() {
-  return (
-    <div>
+    return (
       <div>
-        <h2>Expense Item!</h2>
+        <div>
+          <h2>Expense Item!</h2>
+        </div>
+        <div>
+          <h6>Food Rs 10</h6>
+          <h6>Petrol Rs 100</h6>
+          <h6>Movie Rs 200</h6>
+        </div>
       </div>
-      <div>
-        <h6>Food Rs 10</h6>
-        <h6>Petrol Rs 100</h6>
-        <h6>Movie Rs 200</h6>
-      </div>
-    </div>
-  );
-}
-export default ExpenseItem;
+    );
+  }
+  export default ExpenseItem;
