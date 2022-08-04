9280bad0e96410dc4d5beed9738c3f279c394f0c
Abhishek <abhibaba007@gmail.com>
2022-08-05 01:17:32 +0530

    insert del button

--- src/Component/Expense/ExpenseItem.js
+++ src/Component/Expense/ExpenseItem.js
@@ -4,15 +4,21 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails"
 
-// import Card from "./Card";
+// import Card from "../UI/Card";
 
 const ExpenseItem=(props)=> { 
+  const eventHandler = () =>{
+    let ele=document.querySelector('.expense-item');
+    ele.parentNode.removeChild(ele);
+  }
   return ( 
     <div className="expense-item">
       <ExpenseDate date={props.date}/>
       <ExpenseDetails title ={props.title}/>
-      <ExpenseDetails amount= {props.amount}/>      
+      <ExpenseDetails amount= {props.amount}/>  
+      <button onClick={eventHandler}>Delete</button>    
     </div>
+    
   );
   }
 export default ExpenseItem;
